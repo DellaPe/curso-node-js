859987210e543307162b9ecfd8b801eda908a5f2
Pedro Dellatorre <dellatorre.pe@gmail.com>
2023-10-18 19:11:16 -0300

    Tal cual como las clases

--- Clase5/servers/serverMongodb.js
+++ Clase5/servers/serverMongodb.js
@@ -3,11 +3,10 @@ import { createApp } from '../app.js'
 import mongoose from 'mongoose'
 // Models
 import { MovieModel } from '../models/mongodb/movie.js'
-import { UsersModel } from '../models/mongodb/users.js'
 
 const connectionAndCreateApp = async () => {
   await mongoose.connect(process.env.MONGO_DB_URL)
-  createApp({ movieModel: MovieModel, usersModel: UsersModel })
+  createApp({ movieModel: MovieModel })
 }
 
 connectionAndCreateApp()
