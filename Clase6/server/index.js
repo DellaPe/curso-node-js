a2af2b5f52ac8204c679ef7753f25b9585b83897
Pedro Dellatorre <dellatorre.pe@gmail.com>
2023-10-18 22:00:04 -0300

    Inicio clase 6

---
+++ Clase6/server/index.js
@@ -0,0 +1,35 @@
+import 'dotenv/config'
+import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+app.use(logger('dev'))
+
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+    console.log('One user disconnected')
+  })
+
+  // socket.on('message', (message) => {
+  //   console.log(message)
+  //   io.emit('message', message)
+  // })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
